c94bbcb88793e1a4623ad527f6342e135f3a6410
TigerClips1 <tigerclips1@localhost.localdomain>
2023-06-01 01:45:08 -0400

    Learning C++

---
+++ Main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+using namespace std;
+
+//intro
+
+/*int main() { 
+    cout << "Hello World!" << endl;
+    return 0;
+}
+*/
+
+//Drawing shape using print satement
+
+/*int main2 () 
+{
+    cout << "   / |" << endl; //cout mean console out and endl mean end line in C++ terms
+    cout << "  /  |" << endl;
+    cout << " /   |" << endl;
+    cout << "/_ _ |" << endl;
+
+    return 0;
+}
+*/
+
+//Variables
+
+/*int main3() {
+    
+    *string CharacterName = "Tom";
+    *int CharacterAge;
+    *CharacterAge = 50;
+    *
+    *cout << "There once was a man name " << CharacterName << endl;
+    *cout << "He was "<< CharacterAge << " years old" << endl;
+    *
+    *CharacterName = "Mike";
+    *cout << "He liked the name " << CharacterName << endl;
+    *cout << "But did not like being " << CharacterAge << endl;
+    *
+    *return 0;
+*}
+*/
+
+//Data Types
+
+/*int main4()
+{
+
+    char grade = "A";
+    string phrase = "Giraffe Academy";
+    int age = 50;
+    int age_negtive = -50;
+    float e = 25; //float in python C/C++ i just don't get fun fact
+    double gpa = 4.5;
+    bool isMale = true; // or false bool is bool like python
+    
+    /*
+    all data type i found online about C++
+    
+    short int  2-32,768 to 32,767
+    
+    unsigned short int 2 0 to 65,535
+    
+    unsigned int 4 0 to 4,294,967,295
+    
+    int 4 -2,147,483,648 to 2,147,483,647
+    
+    long int 4 -2,147,483,648 to 2,147,483,647
+    
+    unsigned long int 4 0 to 4,294,967,295
+    
+    long long int 8 -(2^63) to (2^63)-1
+    
+    unsigned long long int 8 0 to 18,446,744,073,709,551,615
+    
+    signed char 1 -128 to 127
+    
+    unsigned char 1 0 to 255
+
+    float 4 -3.4×10^38 to 3.4×10^38
+
+    double 8-1.7×10^308 to1.7×10^308
+
+    long double 12-1.1×10^4932 to1.1×10^4932
+
+    wchar_t 2 or 4 1 wide character
+    
+
+    return 0;
+}
+*/
+
+// Working with Strings
+
+/*int main4()
+*{
+    *string phrase = "Giraffe Academy";
+    *string phrasesub;
+    *phrasesub = phrase.substr(8, 3 );
+    *or you can use phrase.length() to see the full text
+    *phrase [0] = 'w';
+    *cout << phrase.find("Academy", 0) << endl; //endl is \n but high C++ cout make so much stuff easy
+    *cout << phrasesub << endl;
+    *cout << "Hello" << endl;
+
+    return 0;
+*}
+*/
+
+// Working With Numbers
+/*TODO 49:02*/
+int main()
+{
+    return 0;
+}
